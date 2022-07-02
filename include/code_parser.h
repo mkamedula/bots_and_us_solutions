34cc211813937ba6fdc1e623b98c4433f733c1f7
Małgosia <m.kamedula@precisionrobotics.com>
2022-07-02 16:36:29 +0100

    Add initial file structure and CMake related files

---
+++ include/code_parser.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace example
+{
+    class CodeParser
+    {
+      private:
+        void test();
+    };
+}
---
+++ src/code_parser.cpp
@@ -0,0 +1,11 @@
+#include "code_parser.h"
+
+#include "iostream"
+
+namespace example
+{
+    void CodeParser::test()
+    {
+        std::cout << "test" << std::endl;
+    }
+} // namespace example
