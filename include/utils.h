d0d9e86054bd37b33315eabcb19fbf786b89fa68
Małgosia <m.kamedula@precisionrobotics.com>
2022-07-03 10:37:29 +0100

    Remove led related methods  from utils

--- include/utils.h
+++ include/utils.h
@@ -1,15 +1,14 @@
 #pragma once
 
-#include <array>
 #include <optional>
-#include <unordered_map>
+#include <png.h>
 
 namespace botsAndUs
 {
     std::optional<uint32_t> getMod97(std::string_view code);
 
-    [[nodiscard]] bool getBitCode(char id, std::array<bool,8>& code);
+    template<typename Container>
+    [[nodiscard]] bool savePng(const Container& data, std::string_view file_name);
 
-    [[nodiscard]] bool getPixelCode(std::string_view code, std::array<bool, 256>& pixelCode);
 
 }
