badc587fbedf254833d2c1a5748b4d0c82823207
Małgosia <m.kamedula@precisionrobotics.com>
2022-07-05 23:40:29 +0100

    Update comments in the utils.cpp

--- include/utils.h
+++ include/utils.h
@@ -10,11 +10,15 @@ namespace xxxDisplay
 {
 
 /**
- * @brief Compute a mod 97 checksum of a given numerical string
- * @param code a string containing only numerical characters of maximum length of 9 characters
- * @return computed checksum or a fail mode
+ * @brief Compute a mod 97 checksum of a given numerical string. Only up to 9-digit inputs are supported at the moment.
+ *
+ * @param code A string containing only numerical characters of maximum length of 9 characters.
+ * @return Computed checksum
+ *
+ * @throw std::out_of_range Received too long value, only up to 9-digits is supported at the moment.
+ * @throw std::invalid_argument Received unsupported character in the input string. Only numeric characters are supported.
  */
-std::optional<uint32_t> getMod97(const std::string& code);
+uint32_t getMod97(const std::string& code);
 
 /**
  * @brief Save a 1-bit depth, greyscale PNG image from provided pixel data.
@@ -26,9 +30,24 @@ std::optional<uint32_t> getMod97(const std::string& code);
  * @tparam Container a random access ordered data structure that holds a 1-byte of data per element
  * @param data pointer to the container with the pixel data
  * @param file_name name of the generated PNG image (with path)
+ *
+ * @throw SaveFileException Could not open the file for saving.
+ * @throw LibpngException Encounter an internal error from libpng.
  */
 template<typename Container>
 void savePngRowImage(const Container& data, [[maybe_unused]] const std::string& file_name);
+
+/**
+ * @brief Save a 1-bit depth, greyscale PNG image from provided pixel data.
+ *
+ * @param data pointer to the container with the pixel data
+ * @param file_name name of the generated PNG image (with path)
+ * @param width a width of the image to be saved in pixels
+ *
+ * @throw SaveFileException Could not open the file for saving.
+ * @throw LibpngException Encounter an internal error from libpng.
+ */
+void savePngImp(unsigned char& data, const std::string& file_name, size_t width);
 
 }
 
