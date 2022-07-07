41451a3443a399278538c65a6ce53e1f88127a33
Małgosia <m.kamedula@precisionrobotics.com>
2022-07-07 22:36:54 +0100

    Remove declaration of save png implementation from utils

--- include/utils.h
+++ include/utils.h
@@ -21,29 +21,5 @@ namespace xxxDisplay
  */
 uint32_t getMod97(const std::string& code);
 
-/**
- * Save a PNG image from provided 1-D pixel data. Created image will be 1-bit depth, greyscale and 1-bit high.
- * Each entry in the container will be cast to unsigned char, and so only first byte of data is significant.
- * Later bytes in the provided data will be ignored.
- *
- * The created image will have a width equal to eight times the size of the container minus value of ignore_bits
- * parameter. The ignore_bits parameter allow to generate images of any length. The value of ignore_bits should
- * be smaller than 8*data.size().
- *
- * @tparam Container a random access ordered data structure that holds a 1-byte of data per element
- * @param data 1-D container with pixel data
- * @param file_name name of the generated PNG image (with path). The parent directory must exists or
- * SaveFileException will be thrown.
- * @param ignored_bits number of bits to be ignored from the end of the stream. This value should be smaller than
- * 8*data.size().
- *
- * @throw SaveFileException Could not open the file for saving or and empty image has been requested.
- * @throw LibpngException Encounter an internal error from libpng.
- *
- */
-template<typename Container>
-void save1DPng(const Container& data, const std::string& file_name, size_t ignored_bits);
 
 }
-
-#include "utils.tpp"
